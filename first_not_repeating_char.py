da0955611b0db9f546bccd02cd44c78138fe5548
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2017-11-26 07:49:26 -0500

    probando los comentarios y subida desde la opcion se vcode para git

--- first_not_repeating_char.py
+++ first_not_repeating_char.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8  -*-
+"""
+El objetivo de este algorimo es encontrar el primer caracter que no se repite 
+en una secuencia de caracteres.  En caso the que todos esten repetidos, lo 
+informará y terminará el programa.
+
+Ejemplo:
+
+|       secuencia       |   caracter repetido   |
+| abavabad              |           v           |
+| atgcaatgcccata        |           _ (NO HAY)  |
+
+"""
+
+def first_not_repeating_char(char_sequence):
+    pass
+
+
+
 
 
 if __name__ == '__main__':
