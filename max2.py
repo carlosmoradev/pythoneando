93bc4616950f4559f5da8d03a432de8208db5ed4
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2017-11-28 06:59:10 -0500

    codigo basico para comenzar con las pruebas

---
+++ max2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+def numax(p, s):
+    if p > s:
+        print("n1 es mayor que n2")
+    elif p < s:
+        print "n2 es mayor que n1"
+    else:
+        print("son iguales")
+
+numax(3, 6)
